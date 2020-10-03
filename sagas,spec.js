542db63dcabb457d4d86f428db0029855069edda
Nathalia Rus <nathaliarus@gmail.com>
2020-10-03 13:38:05 +0100

    assert

--- sagas,spec.js
+++ sagas,spec.js
@@ -4,4 +4,10 @@ import { incrementAsync } from "./sagas";
 
 test("incrementAsync Saga test", (assert) => {
   const gen = incrementAsync();
+
+  assert.deepEqual(
+    gen.next(),
+    { done: false, value: undefined },
+    "incrementAsync should return a Promise that will resolve after 1 second"
+  );
 });
