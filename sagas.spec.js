67ffa2777f519cc642530bd1c7be963fe6dcb2bf
Nathalia Rus <nathaliarus@gmail.com>
2020-10-03 14:45:35 +0100

    import delay used

--- sagas.spec.js
+++ sagas.spec.js
@@ -1,6 +1,6 @@
 import test from "tape";
 import { put, call } from "redux-saga/effects";
-import { incrementAsync } from "./sagas";
+import { incrementAsync, delay } from "./sagas";
 
 test("incrementAsync Saga test", (assert) => {
   const gen = incrementAsync();
