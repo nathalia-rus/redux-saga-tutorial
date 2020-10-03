c60d6718fc164af552482cc23cfa5308f43830e9
Nathalia Rus <nathaliarus@gmail.com>
2020-10-03 13:19:35 +0100

    write watchIncrementAsync saga

--- sagas.js
+++ sagas.js
@@ -1,3 +1,15 @@
+import { put, takeEvery } from "redux-saga/effects";
+
 export function* helloSaga() {
   console.log("Hello Saga!");
+}
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: "INCREMENT" });
+}
+
+export function* watchIncrementAsync() {
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
 }
