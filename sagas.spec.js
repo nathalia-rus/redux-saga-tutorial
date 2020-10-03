595862b1b9f8cf4d392b8471169fdd7bee252abb
Nathalia Rus <nathaliarus@gmail.com>
2020-10-03 14:43:40 +0100

    test done

--- sagas.spec.js
+++ sagas.spec.js
@@ -6,8 +6,22 @@ test("incrementAsync Saga test", (assert) => {
   const gen = incrementAsync();
 
   assert.deepEqual(
-    gen.next(),
-    { done: false, value: undefined },
-    "incrementAsync should return a Promise that will resolve after 1 second"
+    gen.next().value,
+    call(delay, 1000),
+    "incrementAsync Saga must call delay(1000)"
   );
+
+  assert.deepEqual(
+    gen.next().value,
+    put({ type: "INCREMENT" }),
+    "incrementAsync saga must dispatch an INCREMENT action"
+  );
+
+  assert.deepEqual(
+    gen.next(),
+    { done: true, value: undefined },
+    "incrementAsync Saga must be done"
+  );
+
+  assert.end();
 });
